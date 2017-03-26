f60c9b56a98eafc2f7d1948569dd5834e9627b5a
Anthony Ngene <anthony.ngene@andela.com>
2017-03-26 18:22:53 +0100

    implement the InvertedIndex class and solve the problem

--- src/js/invertedIndex.js
+++ src/js/invertedIndex.js
@@ -9,16 +9,27 @@ class InvertedIndex {
    */
   constructor() {
     this.files = [];
+    this.showAllFiles = false;
+    this.indexed = {};
+    this.showAllFilesSearch = false;
+    this.searchAllResult = {};
   }
 
-   /**
+  /**
    * Validate file content
    * @function
    * @param {Array} fileContent content of uploaded file
    * @return {Array} valid fileContent or false if invalid file
    */
   static validateFile(fileContent) {
-
+    if (fileContent.constructor !== Array || fileContent.length < 1) return false;
+    for (let i = 0; i < fileContent.length; i += 1) {
+      const title = fileContent[i].title;
+      const text = fileContent[i].text;
+      if (title === undefined || text === undefined) return false;
+      if (text.length < 1) return false;
+    }
+    return fileContent;
   }
 
   /**
@@ -28,7 +39,9 @@ class InvertedIndex {
    * @return {Array} An array of unique words in the text
    */
   static tokenize(text) {
-
+    return text.replace(/[^\w\s-]/g, '')
+      .toLowerCase().split(/\s+/)
+      .filter((word, index, collection) => collection.indexOf(word) === index);
   }
 
   /**
@@ -38,8 +51,28 @@ class InvertedIndex {
    * @param {String} filename name of uploaded file
    * @return {boolean} true if file index was successfully created, else false
    */
-  static createIndex(fileContent, filename) {
+  createIndex(fileContent, filename) {
+    if (this.files.indexOf(filename) !== -1) return false;
+    const result = { bookTitles: [], words: {}, allWords: [] };
+    const fileLength = fileContent.length;
+    for (let i = 0; i < fileLength; i += 1) {
+      result.bookTitles.push(fileContent[i].title);
+      const words = InvertedIndex.tokenize(fileContent[i].text);
 
+      for (let j = 0; j < words.length; j += 1) {
+        const word = words[j];
+        if (result.words[word] === undefined) {
+          result.allWords.push(word);
+          result.words[word] = new Array(fileLength).fill(false);
+        }
+        result.words[word][i] = true;
+      }
+    }
+    this.showAllFiles = false;
+    this.files.push(filename);
+    this.indexed = result;
+    this[filename] = result;
+    return true;
   }
 
   /**
@@ -48,8 +81,11 @@ class InvertedIndex {
    * @param {Object} filename filen
    * @return {Array} Current indexed object (used by angular to display the content
    */
-  static getIndex(filename) {
-
+  getIndex(filename) {
+    if (this[filename] === undefined) return false;
+    this.showAllFiles = false;
+    this.indexed = this[filename];
+    return this.indexed;
   }
 
   /**
@@ -59,8 +95,21 @@ class InvertedIndex {
    * @param {Object} filename name of indexed file where the search will be performed
    * @return {Object} search result or false if nothing was found
    */
-  static searchIndex(searchKey, filename) {
-
+  searchIndex(searchKey, filename) {
+    if (searchKey === undefined || searchKey.length < 1 || this[filename] === undefined) {
+      return false;
+    }
+    const searchTerms = InvertedIndex.tokenize(searchKey);
+    const result = { bookTitles: [], words: {}, allWords: [] };
+    searchTerms.forEach((word) => {
+      if (this[filename].words[word] !== undefined) {
+        result.allWords.push(word);
+        result.words[word] = this[filename].words[word];
+      }
+    });
+    if (result.allWords.length < 1) return false;
+    result.bookTitles = this[filename].bookTitles;
+    return result;
   }
 
   /**
@@ -69,8 +118,17 @@ class InvertedIndex {
    * @param {String} searchKey string containing word(s) to be searched for
    * @return {Object} search result or false if nothing was found
    */
-  static searchAll(searchKey) {
-
+  searchAll(searchKey) {
+    const searchAllResult = { files: [] };
+    this.files.forEach((filename) => {
+      const result = this.searchIndex(searchKey, filename);
+      if (result) {
+        searchAllResult.files.push(filename);
+        searchAllResult[filename] = result;
+      }
+    });
+    if (searchAllResult.files.length < 1) return false;
+    return searchAllResult;
   }
 
   /**
@@ -79,7 +137,12 @@ class InvertedIndex {
    * @param {String} filename name of file to be deleted
    * @return {boolean} true if file was successfully deleted, else false
    */
-  static deleteFile(filename) {
-
+  deleteFile(filename) {
+    if (this[filename] === undefined) return false;
+    this.showAllFilesSearch = false;
+    this.showAllFiles = true;
+    this[filename] = undefined;
+    this.files.splice(this.files.indexOf(filename), 1);
+    return true;
   }
 }
